9a9ab59431019000dd51be0274093374bd234b3b
Aleksandr Kravchenko <svhelpp@gmail.com>
2022-12-03 18:55:20 +0100

    - stream list data;

---
+++ src/components/Main/Streams/StreamInfo.tsx
@@ -0,0 +1,26 @@
+import { Avatar, Card } from "antd"
+import Meta from "antd/es/card/Meta"
+import { IStreamInfo, useGetUsersQuery } from "api/twitchApi"
+
+interface IStreamInfoProps {
+    stream: IStreamInfo;
+}
+
+export const StreamInfo = (props: IStreamInfoProps) => {
+    const { stream } = props;
+
+    const { data: streamerData, isLoading } = useGetUsersQuery({ id: stream.user_id });
+
+    return (
+        <Card
+            style={{ width: 500, margin: "4px" }}
+            hoverable
+        >
+            <Meta
+                avatar={<Avatar src={streamerData?.data[0].profile_image_url} />}
+                title={stream.user_name}
+                description={stream.title}
+                />
+        </Card>
+    )
+}
