c47ef6d2720ea68973d9b00725d506b5774abdb7
Aleksandr Kravchenko <svhelpp@gmail.com>
2022-12-10 22:42:41 +0100

    - close with tray implemented;

--- src/main/util.ts
+++ src/main/util.ts
@@ -1,8 +1,17 @@
 /* eslint import/prefer-default-export: off */
 import { URL } from 'url';
 import path from 'path';
+import { App } from 'electron';
 
-export function resolveHtmlPath(htmlFileName: string) {
+export const getAssetPath = (app: App, ...paths: string[]): string => {
+  const RESOURCES_PATH = app.isPackaged
+    ? path.join(process.resourcesPath, 'assets')
+    : path.join(__dirname, '../../assets');
+
+  return path.join(RESOURCES_PATH, ...paths);
+};
+
+export const resolveHtmlPath = (htmlFileName: string) => {
   if (process.env.NODE_ENV === 'development') {
     const port = process.env.PORT || 1212;
     const url = new URL(`http://localhost:${port}`);
