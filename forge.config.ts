6018573762fbc42ea961e666e1c3706b6017275a
Aleksandr Kravchenko <svhelpp@gmail.com>
2023-09-03 17:17:53 +0200

    - added publisher settings;

--- forge.config.ts
+++ forge.config.ts
@@ -19,6 +19,18 @@ const config: ForgeConfig = {
   },
   rebuildConfig: {},
   makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'svhelp',
+          name: 'TwitchNotifierDesktop'
+        },
+        prerelease: true
+      }
+    }
+  ],
   plugins: [
     new AutoUnpackNativesPlugin({}),
     new WebpackPlugin({
