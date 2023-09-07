68c37fe4359d706c7f3aa28f8d0451bf64e6312f
Aleksandr Kravchenko <svhelpp@gmail.com>
2023-09-07 17:08:40 +0200

    - fixed login page styles; - adjusted version;

--- src/renderer/components/Login/LoginPage.tsx
+++ src/renderer/components/Login/LoginPage.tsx
@@ -1,5 +1,6 @@
 import { Button } from "antd";
 import { useAuthRedirect } from "renderer/customHooks/useAuthRedirect";
+import styled from "styled-components";
 
 export const LoginPage = () => {
     useAuthRedirect();
@@ -11,8 +12,19 @@ export const LoginPage = () => {
       `https://id.twitch.tv/oauth2/authorize?response_type=${responseType}&client_id=${process.env.TWITCH_API_KEY}&redirect_uri=${home_url}&scope=${scope}`;
       
     return (
+      <LoginContainer>
         <Button type="primary" href={authLink} target="_blank">
           Log in with Twitch
         </Button>
+      </LoginContainer>
     );
 }
+
+const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  min-height: 300px;
+`
